c5db3755d4b66da11d78165ca689132fc78ff01c
Vivek Chandra Ojha <viveknepa@Viveks-Mac-mini.local>
2026-03-13 11:16:21 +0545

    Job for importing client

---
+++ app/Jobs/ImportClientsJob.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ImportClientsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $path;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle() {}
+}
